7cca92bbfa6d5b1b1f35e3fb11d15db08a4fe1ae
FrontiMaxim <87124403+FrontiMaxim@users.noreply.github.com>
2021-07-25 20:12:55 +0400

    Implementation of navigation buttons on the player and partial code improvement

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ let pointer = document.querySelector('.pointer');
 let Library = document.querySelector('.Library');
 let Player = document.querySelector('.Player');
 
+let currentId = 0;
+
 let musics = [
     {
         id: 0,
@@ -42,16 +44,21 @@ musics.map(item => {
 });
 
 
+let arrayMusic = document.querySelectorAll('.music');
+let elementFocus = arrayMusic[currentId];
+
+
 let imgMusic = document.querySelector('.img-music');
 let audioName = document.querySelector('.audio-name');
 let audioPerformers = document.querySelector('.audio-perfomers');
 
 
 window.addEventListener('load', function () {
-    audio.src =  musics[0].src_audio;
-    imgMusic.src = musics[0].src_img;
-    audioName.textContent = musics[0].name;
-    audioPerformers.textContent = musics[0].performers;
+    elementFocus.classList.add('focus');
+    audio.src =  musics[currentId].src_audio;
+    imgMusic.src = musics[currentId].src_img;
+    audioName.textContent = musics[currentId].name;
+    audioPerformers.textContent = musics[currentId].performers;
 });
 
 // события на раскрытия плейлиста
@@ -64,57 +71,68 @@ btnLibrary.addEventListener('click', function(e) {
 
 // создаём событие на воспроизведение мелодии по выбору музыки из плейлиста
 let audio = document.querySelector('audio');
-let arrayMusic = document.querySelectorAll('.music');
+let btnPlay = document.querySelector('.btn-play');
 arrayMusic.forEach(item => {
     item.addEventListener('click', function(event) { 
-        // ищем объект с нужным id и элементы audio даём ссылку на мелодию из этого же объекта
-        let music = musics.find(music => music.id === parseInt(event.currentTarget.id, 10));
+       
+        currentId = parseInt(event.currentTarget.id, 10);
 
-        audio.src =  music.src_audio;
-        imgMusic.src = music.src_img;
-        audioName.textContent = music.name;
-        audioPerformers.textContent = music.performers;
-        
-        // проверка на повторное нажатие
-        // если на элементе фокус (то есть с этого элемента воспроизводится музыка)
-        // то останавливаем музыку и снимаем фокус с этого элемента
-        if(document.getElementById(`${event.currentTarget.id}`).classList.contains('focus')) {
-            document.getElementById(`${event.currentTarget.id}`).classList.remove('focus');
-            btnPlay.src = 'https://s2.svgbox.net/hero-solid.svg?ic=play&color=000000';
-            audio.pause();
-            audio.classList.remove('start');
-        } else {
-            // если нужный элемент не соержит фокуса, то устанавливаем его на нём и воспроизводим мелодию
-            if(document.querySelector(`.focus`)) {
-                document.querySelector(`.focus`).classList.remove('focus');
+        newElementFocus = event.currentTarget; // берём элемент, на который мы нажали
+
+        // если прошлый элемент и новый окажутся одинаковыми
+        if (newElementFocus.id === elementFocus.id) {
+            // то проверяем на повторное нажатие через класс start и при необходимости его убираем или добавляем
+            if (audio.classList.contains('start')) {
+                btnPlay.src = 'https://s2.svgbox.net/hero-solid.svg?ic=play&color=000000';
+                audio.classList.remove('start');
+                audio.pause();
+                imgMusic.classList.remove('rotate-img');
+            } else {
+                btnPlay.src = 'https://s2.svgbox.net/hero-solid.svg?ic=pause&color=000000';
+                audio.classList.add('start');
+                audio.play();
+                imgMusic.classList.add('rotate-img');
             }
-            audio.play();
-            audio.classList.add('start');
+        } else {
+
+            audio.src =  musics[currentId].src_audio;
+            imgMusic.src = musics[currentId].src_img;
+            audioName.textContent = musics[currentId].name;
+            audioPerformers.textContent = musics[currentId].performers;
+            
             btnPlay.src = 'https://s2.svgbox.net/hero-solid.svg?ic=pause&color=000000';
-            document.getElementById(`${event.currentTarget.id}`).classList.add('focus');
+
+            elementFocus.classList.remove('focus'); // убираем focus у прошлого элемента
+            elementFocus = newElementFocus;
+            elementFocus.classList.add('focus');
+
+            audio.classList.add('start');
+            audio.play();
+            imgMusic.classList.add('rotate-img');
         }
     })
-})
+});
 
 
 /* модернизация кода:
    1) создание функции для проигрывания мелодии и устнановки её на паузу
    3) реализации работы других кнопок
 */
+
 // реализация проигрования мелодии
-let btnPlay = document.querySelector('.btn-play');
 btnPlay.addEventListener('click', function() {
-    // если у проигрывателя нет класса start, то устанавливаем его меняя вид и запускаю audio
-    if(!audio.classList.contains('start')) {
-        btnPlay.src = 'https://s2.svgbox.net/hero-solid.svg?ic=pause&color=000000';
-        audio.play();
-        audio.classList.add('start');
-    } else {
+    if(audio.classList.contains('start')) {
         btnPlay.src = 'https://s2.svgbox.net/hero-solid.svg?ic=play&color=000000';
-        audio.pause();
         audio.classList.remove('start');
+        audio.pause();
+        imgMusic.classList.remove('rotate-img');
+    } else {
+        btnPlay.src = 'https://s2.svgbox.net/hero-solid.svg?ic=pause&color=000000';
+        audio.classList.add('start');
+        audio.play();
+        imgMusic.classList.add('rotate-img');
     }
-})
+});
 
 
 // реализация прогресса проигрования мелодии
@@ -124,11 +142,13 @@ audio.addEventListener('timeupdate', function() {
     progress.style.width = position * 100 + '%';
 });
 
+
 // возвращаем прогресс в 0 позицию при окончании прогрывания мелодии
 audio.addEventListener('ended', function() {
     progress.style.width = 0 + '%';
     btnPlay.src = 'https://s2.svgbox.net/hero-solid.svg?ic=play&color=000000';
 });
+
 
 let btnRepeat = document.querySelector('.btn-repeat');
 btnRepeat.addEventListener('click', function() {
@@ -139,5 +159,56 @@ btnRepeat.addEventListener('click', function() {
         btnRepeat.src = 'https://s2.svgbox.net/hero-solid.svg?ic=refresh&color=D3B9FF';
         audio.setAttribute('loop', 'loop');
     }
-})
+});
+
+
+let btnBack = document.querySelector('.btn-back');
+let btnNext = document.querySelector('.btn-next');
+
+btnBack.addEventListener('click', function() {
+
+    if ((currentId - 1) < 0) {
+        currentId = musics.length - 1;
+    } else {
+        currentId -= 1;
+    }
+    
+    elementFocus.classList.remove('focus');
+    elementFocus = arrayMusic[currentId];
+    elementFocus.classList.add('focus');
+
+    audio.src =  musics[currentId].src_audio;
+    imgMusic.src = musics[currentId].src_img;
+    audioName.textContent = musics[currentId].name;
+    audioPerformers.textContent = musics[currentId].performers;
+    
+    if (audio.classList.contains('start')) {
+        audio.play();
+        imgMusic.classList.add('rotate-img');
+    }
+});
+
+btnNext.addEventListener('click', function() {
+
+    if ((currentId + 1) > musics.length - 1) {
+        currentId = 0;
+    } else {
+        currentId += 1;
+    }
+    
+    elementFocus.classList.remove('focus');
+    elementFocus = arrayMusic[currentId];
+    elementFocus.classList.add('focus');
+
+    audio.src =  musics[currentId].src_audio;
+    imgMusic.src = musics[currentId].src_img;
+    audioName.textContent = musics[currentId].name;
+    audioPerformers.textContent = musics[currentId].performers;
+      
+    if (audio.classList.contains('start')) {
+        audio.play();
+        imgMusic.classList.add('rotate-img');
+    }
+});
+
 
